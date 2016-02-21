f0399f07575128782c3c5abc584b7a5f43221652
Juan Luis Ramirez Vaquero <juanlu@JUANLU-PC>
2016-02-21 14:52:56 +0100

    He modificado el fichero README.md

--- Ejercicio_XML.py
+++ Ejercicio_XML.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-
 from lxml import etree
-arbol=etree.parse('procesion.xml')
 
+arbol=etree.parse('procesion.xml')
 raiz=arbol.getroot()
 
-procesiones=raiz.findall("/resultado/result")
+procesiones=raiz.xpath("//procesion")
 
+#3)Pedir por teclado el inicio del campo cofradia, (Por ejemplo buscar todas las cofradias que empiecen por "Hermandad").
+
+cofradia=raw_input("Dime el inicio del nombre de la hermandad: ")
+
+for cofradias in procesiones:
+	if cofradias.find("cofradia").text.startswith(cofradia.title()):
+		print cofradias.find("cofradia").text
